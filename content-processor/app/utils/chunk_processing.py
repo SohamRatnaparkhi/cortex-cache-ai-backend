e4a4b45f4fad4e8726f2ec8613023ba91da7eb4a
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2024-11-02 20:32:16 +0530

    feat: implement open ai chat completions

--- content-processor/app/utils/chunk_processing.py
+++ content-processor/app/utils/chunk_processing.py
@@ -7,15 +7,21 @@ from typing import Dict, List, Optional, Tuple
 
 from anthropic import Anthropic
 from dotenv import load_dotenv
+from openai import OpenAI
 from pydantic import BaseModel
 
 load_dotenv()
 MAX_CHUNK_SIZE = 20
 CONTEXT_WINDOW_SIZE = 40
 
-client = Anthropic(
+anthropic_client = Anthropic(
     api_key=os.getenv("ANTHROPIC_API_KEY"),
     max_retries=3
+)
+
+openai_client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    max_retries=3,
 )
 
 BULK_CONTEXT_PROMPT = """
@@ -36,7 +42,7 @@ You will be given the following inputs:
 For each chunk, your responsibilities are as follows:
 
 1. Contextual Analysis: Examine the chunk's relevance within the overall document context.
-2. Description Creation: Write a short, precise description of how the chunk relates to the document's main theme. Include important contextual elements such as section headings or key points, product names, etc. if applicable. 
+2. Description Creation: Write a short, precise description of how the chunk relates to the document's main theme. Include important contextual elements such as section headings or key points, product names, etc. if applicable.
 3. Search Relevance: Ensure the description will enhance the retrievability of this chunk for a search query.
 4. Restrict description to 3 to 4 sentences
 
@@ -57,6 +63,59 @@ Remember, the goal is to create descriptions that will help in building context-
 
 Provide your output in JSON format without any additional commentary or XML tags.
 """
+EXAMPLE = """
+<examples>
+<example>
+<CONTEXT>
+Climate change refers to long-term shifts in temperature and weather, mainly caused by human activities like burning fossil fuels. It has wide-ranging effects on ecosystems and societies due to the greenhouse effect.
+</CONTEXT>
+<sentence1>
+The IPCC was founded in 1988.
+</sentence1>
+<sentence2>
+Greenhouse gases, such as carbon dioxide and methane, trap heat in the atmosphere.
+</sentence2>
+<sentence3>
+Rising temperatures are melting polar ice and glaciers.
+</sentence3>
+<sentence4>
+Some areas may see more rain, while others face droughts.
+</sentence4>
+<sentence5>
+The Paris Agreement aims to limit global warming to 1.5°C-2°C above pre-industrial levels.
+</sentence5>
+<sentence6>
+Renewable energy, like solar and wind, helps reduce emissions.
+</sentence6>
+<sentence7>
+Many species are struggling to adapt to rapid environmental changes.
+</sentence7>
+<sentence8>
+Extreme weather events are becoming more severe due to climate change.
+</sentence8>
+<sentence9>
+Carbon pricing, such as taxes and cap-and-trade, incentivizes emission reductions.
+</sentence9>
+<sentence10>
+Adaptation strategies include drought-resistant crops and flood defenses.
+</sentence10>
+<ideal_output>
+{
+    "sentence1": "Establishes global climate science and policy organization.",
+    "sentence2": "Explains the greenhouse effect's role in climate change.",
+    "sentence3": "Describes impacts of rising temperatures on polar regions.",
+    "sentence4": "Highlights uneven regional impacts of climate change.",
+    "sentence5": "Outlines global temperature targets in the Paris Agreement.",
+    "sentence6": "Focuses on clean energy solutions for reducing emissions.",
+    "sentence7": "Explains biodiversity loss due to rapid environmental changes.",
+    "sentence8": "Details increasing extreme weather linked to climate change.",
+    "sentence9": "Describes economic tools to reduce greenhouse gas emissions.",
+    "sentence10": "Explains practical adaptation strategies for climate resilience."
+}
+</ideal_output>
+</example>
+</examples>
+"""
 
 
 class OutputModelStructure(BaseModel):
@@ -70,6 +129,52 @@ class OutputModelStructure(BaseModel):
     sentence8: Optional[str] = ""
     sentence9: Optional[str] = ""
     sentence10: Optional[str] = ""
+
+
+def get_context_summary_from_openai(context: str, sentences: List[str]) -> OutputModelStructure:
+    res = None
+    try:
+        sentences_xml = "\n".join([
+            f"    <sentence{i+1}> {sentence} </sentence{i+1}>"
+            for i, sentence in enumerate(sentences)
+        ])
+
+        ideal_output = ""
+
+        for i in range(len(sentences)):
+            ideal_output += f"\"sentence{i+1}\": \"Description of the sentence {i+1}.\",\n"
+
+        ideal_output = "{\n" + ideal_output + "\n}"
+
+        # Create the prompt
+        prompt = BULK_CONTEXT_PROMPT.format(
+            CONTEXT=context, sentences_xml=sentences_xml, IDEAL_OUTPUT=ideal_output)
+
+        response = openai_client.chat.completions.create(
+            model="gpt-4o-mini",
+            temperature=0.0,
+            messages=[
+                {"role": "user", "content": prompt},
+                {"role": "user", "content": EXAMPLE},
+            ],
+        )
+
+        print(response.usage)
+        res = response.choices[0].message.content.strip()
+        res = res.replace("\n", "")
+
+        print("Response")
+        # print(res)
+
+        # convert to json object
+        res = json.loads(res)
+
+        return OutputModelStructure.model_validate(res, strict=False)
+
+        # return res if res != None else manual_parsing(len(sentences), res)
+    except Exception as e:
+        print(f"Error occurred while getting context summary: {e}")
+        return manual_parsing(len(sentences), res)
 
 
 def get_context_summary_from_anthropic(context: str, sentences: List[str]) -> OutputModelStructure:
@@ -86,6 +191,7 @@ def get_context_summary_from_anthropic(context: str, sentences: List[str]) -> Ou
     Raises:
         ValueError: If not exactly 10 sentences are provided
     """
+    res = None
     try:
         # Create the sentences XML block
         sentences_xml = "\n".join([
@@ -104,9 +210,8 @@ def get_context_summary_from_anthropic(context: str, sentences: List[str]) -> Ou
         prompt = BULK_CONTEXT_PROMPT.format(
             CONTEXT=context, sentences_xml=sentences_xml, IDEAL_OUTPUT=ideal_output)
 
-        response = client.beta.prompt_caching.messages.create(
+        response = anthropic_client.beta.prompt_caching.messages.create(
             model="claude-3-haiku-20240307",
-            max_tokens=1024,
             temperature=0.0,
             messages=[
                 {
@@ -114,25 +219,26 @@ def get_context_summary_from_anthropic(context: str, sentences: List[str]) -> Ou
                     "content": [
                         {
                             "type": "text",
-                            "text": "<examples>\n<example>\n<CONTEXT>\nClimate change is a complex global phenomenon characterized by long-term shifts in temperature and weather patterns. Primarily driven by human activities, particularly the burning of fossil fuels, it has far-reaching consequences for the planet's ecosystems and human societies. The main driver of climate change is the greenhouse effect, where gases like carbon dioxide trap heat in the Earth's atmosphere, leading to global warming. This warming trend has numerous impacts, including rising sea levels, more frequent and severe weather events, changes in precipitation patterns, and disruptions to ecosystems and biodiversity. The effects of climate change are not uniform across the globe, with some regions experiencing more dramatic changes than others. Climate scientists use various methods to study these changes, including analyzing historical climate data, using computer models to project future scenarios, and observing current environmental indicators. Addressing climate change requires a multifaceted approach, involving mitigation strategies to reduce greenhouse gas emissions and adaptation measures to cope with the changes already underway. International cooperation, technological innovation, and changes in individual and societal behaviors are all crucial components in the global response to this challenge. As the impacts of climate change become more apparent, there is an increasing urgency to implement effective solutions and build resilience in vulnerable communities and ecosystems.\n</CONTEXT>\n<sentence1>\nThe Intergovernmental Panel on Climate Change (IPCC) was established in 1988 to provide policymakers with regular scientific assessments on climate change, its implications, and potential future risks.\n</sentence1>\n<sentence2>\nGreenhouse gases, such as carbon dioxide, methane, and water vapor, play a crucial role in trapping heat within the Earth's atmosphere, contributing to the greenhouse effect.\n</sentence2>\n<sentence3>\nRising global temperatures have led to the melting of polar ice caps and glaciers, contributing to sea level rise and threatening coastal communities worldwide.\n</sentence3>\n<sentence4>\nClimate models predict that some regions may experience increased precipitation, while others may face more frequent and severe droughts as a result of changing weather patterns.\n</sentence4>\n<sentence5>\nThe Paris Agreement, adopted in 2015, aims to limit global temperature increase to well below 2 degrees Celsius above pre-industrial levels, with efforts to limit the increase to 1.5 degrees Celsius.\n</sentence5>\n<sentence6>\nRenewable energy sources, such as solar and wind power, are becoming increasingly important in efforts to reduce greenhouse gas emissions and mitigate climate change.\n</sentence6>\n<sentence7>\nClimate change is affecting biodiversity, with many species struggling to adapt to rapidly changing habitats and facing increased risk of extinction.\n</sentence7>\n<sentence8>\nExtreme weather events, including hurricanes, heatwaves, and wildfires, are becoming more frequent and intense due to climate change, posing significant risks to human health and safety.\n</sentence8>\n<sentence9>\nCarbon pricing mechanisms, such as carbon taxes and cap-and-trade systems, are economic tools designed to incentivize the reduction of greenhouse gas emissions.\n</sentence9>\n<sentence10>\nClimate change adaptation strategies include developing drought-resistant crops, improving flood defenses, and implementing early warning systems for extreme weather events.\n</sentence10>\n<ideal_output>\n{\n    \"sentence1\": \"Description of a key international scientific organization monitoring climate change. Establishes the institutional framework for global climate science and policy interface, setting the foundation for understanding and addressing climate change impacts.\",\n    \n    \"sentence2\": \"Fundamental explanation of the primary mechanism driving climate change. Details the physical process underlying global warming through atmospheric heat retention by specific gases.\",\n    \n    \"sentence3\": \"Direct observable impact of global warming on Earth's cryosphere and coastal regions. Demonstrates the concrete consequences of temperature increases on sea level rise and human communities.\",\n    \n    \"sentence4\": \"Analysis of regional climate change impacts on precipitation patterns. Highlights the uneven distribution of climate change effects and their implications for water availability across different areas.\",\n    \n    \"sentence5\": \"Key international policy response to climate change threats. Outlines specific global temperature targets agreed upon by the international community to prevent dangerous levels of warming.\",\n    \n    \"sentence6\": \"Solution-focused discussion of alternative energy sources for climate change mitigation. Emphasizes technological approaches to reducing greenhouse gas emissions through clean energy adoption.\",\n    \n    \"sentence7\": \"Environmental impact of climate change on species and ecosystems. Illustrates the biological consequences of rapid environmental changes and their effects on global biodiversity.\",\n    \n    \"sentence8\": \"Description of increasing severe weather events linked to climate change. Details the direct threats to human safety and well-being from climate-related weather phenomena.\",\n    \n    \"sentence9\": \"Economic policy tools for addressing climate change through market mechanisms. Explains approaches to incentivize emissions reduction through financial instruments.\",\n    \n    \"sentence10\": \"Practical adaptation strategies for managing climate change impacts. Outlines specific measures being implemented to build resilience against various climate-related challenges.\"\n}\n</ideal_output>\n</example>\n</examples>\n\n",
+                            "text": EXAMPLE,
                             "cache_control": {"type": "ephemeral"}
                         },
                         {
                             "type": "text",
                             "text": prompt,
-                            "cache_control": {"type": "ephemeral"}
+                            # "cache_control": {"type": "ephemeral"}
                         }
                     ],
                 },
             ],
             extra_headers={"anthropic-beta": "prompt-caching-2024-07-31"}
         )
+        print(response.usage)
         res = response.content[0].text.strip()
         # remove all \n
         res = res.replace("\n", "")
 
         print("Response")
-        print(res)
+        # print(res)
 
         # convert to json object
         res = json.loads(res)
@@ -162,7 +268,7 @@ def update_chunks(chunks: List[str]) -> List[str]:
             context_chunks = chunks[start:end]
             context_text = ",\n".join(context_chunks)
             sentences = chunks[i:i+CURRENT]
-            output = get_context_summary_from_anthropic(
+            output = get_context_summary_from_openai(
                 context_text, sentences)
 
             print("Received")
